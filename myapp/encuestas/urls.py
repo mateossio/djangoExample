42bf140ff52dceb6272a7909a219e0b1cff974f3
Mateo Bossio <mateofbossio@gmail.com>
2020-04-18 00:22:07 -0300

    aaaaaaaaForms

--- myapp/encuestas/urls.py
+++ myapp/encuestas/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'encuesta'
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('<int:question_id>/', views.detail, name='detail'),
+	path('pregunta/<int:question_id>/', views.detail, name='detail'),
 	path('<int:question_id>/results/', views.results, name='results'),
-	path('<int:question_id>/vote/', views.vote, name='vote'),
-	path('<int:question_id>/<int:choice_id>/vote/', views.choice_vote, name='choice_vote'),
+	path('<int:question_id>/vote/', views.form, name='form'),
+	path('<int:question_id>', views.vote, name='vote'),
+	path('home',views.home, name='home'),
+	path('<int:question_id>/results/', views.results, name='results'),
 ]
--- myapp/encuestas/views.py
+++ myapp/encuestas/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from .models import Question, Choice
 # Create your views here.
@@ -16,11 +16,23 @@ def detail(request, question_id):
 	return render(request, 'encuestas/detalle.html',{'question':question})
 
 def results(request, question_id):
-	response = "Estas mirando los resultados de las preguntas %s."
-	return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'encuestas/resultados.html', {'question': question})
+
+def form(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'encuestas/formulario.html', {'question':question})
 
 def vote(request, question_id):
-	return HttpResponse("Estas votando en la pregunta: %s." %question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render (request, 'encuestas/formulario.html', {'question':question, 'error_message': "No elegiste una opcion",})	
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('encuesta:results', args=(question.id,)))
 
 def choice_vote(request,question_id,choice_id):
 	question = get_object_or_404(Question, pk=question_id)
@@ -31,3 +43,6 @@ def choice_vote(request,question_id,choice_id):
 	print("Votaste en: "+choice.choice_text)
 	print(choice.votes)
 	return detail(request,question_id)
+
+def home(request):
+	return render(request, 'encuestas/base.html')
